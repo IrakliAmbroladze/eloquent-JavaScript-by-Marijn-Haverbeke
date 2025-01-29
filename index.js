f587b70462f7bed035bc3e24ae47beeba1c30da9
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-01-29 09:16:53 +0400

    ch.2/ex.2: FizzBuzz - book version

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 for (let i = 1; i <= 100; i++) {
-  if (i % 15 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
+  let output = "";
+  if (i % 3 == 0) output += "Fizz";
+  if (i % 5 == 0) output += "Buzz";
+  console.log(output || i);
 }
