6fac2288552c7949f9bdbc04c0a3040f3fc2662c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-02 16:53:00 +0400

    ch3/Recursion: find target +5 *3

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-function countChar(string, ch) {
-  let counted = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] == ch) {
-      counted += 1;
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ??
+        find(current * 3, `(${history} * 3)`)
+      );
     }
   }
-  return counted;
+  return find(1, "1");
 }
 
-function countBs(string) {
-  return countChar(string, "B");
-}
-
-console.log(countBs("BBC"));
-// → 2
-console.log(countChar("kakkerlak", "k"));
-// → 4
+console.log(findSolution(16));
