42d5d6e99754f0df8adc686dd3d1bdf7211cee70
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-03 09:12:26 +0400

    ch4/push pop

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function findSolution(target) {
-  function find(current, history) {
-    if (current == target) {
-      return history;
-    } else if (current > target) {
-      return null;
-    } else {
-      return (
-        find(current + 5, `(${history} + 5)`) ??
-        find(current * 3, `(${history} * 3)`)
-      );
-    }
-  }
-  return find(1, "1");
-}
-
-console.log(findSolution(16));
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+// console.log(sequence);
+sequence.pop();
+console.log(sequence);
