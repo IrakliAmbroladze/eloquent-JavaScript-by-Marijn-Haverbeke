3c63b23128a13838dc130dc26fece77897b156aa
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-08 16:02:20 +0400

    ch4/some theory staff

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-import "./journal.js";
+// let doh = "Doh";
+// // console.log(typeof doh.toUpperCase);
 
-function tableFor(event, journal) {
-  let table = [0, 0, 0, 0];
-  for (let i = 0; i < journal.length; i++) {
-    let entry = journal[i],
-      index = 0;
-    if (entry.events.includes(event)) index += 1;
-    if (entry.squirrel) index += 2;
-    table[index] += 1;
-  }
-  return table;
-}
+// // console.log(doh.toUpperCase());
 
-console.log(tableFor("pizza", JOURNAL));
+// let sequence = [1, 2, 3];
+// sequence.push(4);
+// sequence.push(4);
+
+// console.log(sequence.push());
+// console.log(sequence.length);
+// console.log(sequence.length == sequence.push());
+
+// let day1 = {
+//   squirrel: false,
+//   events: [" work", "touched tree", "pizza", "running"],
+// };
+
+// console.log(day1.squirrel);
+// console.log(day1.wolf);
+// day1.wolf = false;
+// console.log(day1.wolf);
+
+// let descriptions = { work: "Went to work", "touched tree": "Touched a tree" };
+
+// let anObject = { left: 1, right: 2 };
+
+// console.log(anObject.left);
+// delete anObject.left;
+// console.log(anObject.left);
+// console.log("left" in anObject);
+// console.log("right" in anObject);
+
+// console.log(Object.keys({ x: 0, y: 0, z: 2 }));
+
+// let objectA = { a: 1, b: 2 };
+// Object.assign(objectA, { b: 3, c: 4 });
+// console.log(objectA);
+
+let object1 = { value: 10 };
+let object2 = object1;
+let object3 = { value: 10 };
+
+console.log(object1 == object2);
+console.log(object1 == object3);
+
+object1.value = 15;
+console.log(object2.value);
+console.log(object3.value);
