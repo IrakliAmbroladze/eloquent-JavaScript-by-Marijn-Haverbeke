06f9006368c7c32a5525def23fc33724a789e16c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-14 09:55:48 +0400

    ch4/Optional Property Access

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function max(...numbers) {
-  let result = -Infinity;
-  for (let number of numbers) {
-    if (number > result) {
-      result = number;
-    }
-  }
-  return result;
+function city(object) {
+  return object.address?.city;
 }
 
-console.log(max(2, 4, 1, -6, 4, 2));
-
-let numbers = [4, 6, 2, 4, 7, 8, 2, 2, 7, 3];
-console.log(max(...numbers));
+console.log(city({ address: { city: "Toronto" } }));
+console.log(city({ name: "Vera" }));
