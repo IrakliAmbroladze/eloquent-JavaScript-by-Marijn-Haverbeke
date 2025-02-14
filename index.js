e278d1a54c602cd141ab2d1a35d53bbf90d22f44
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-14 12:02:24 +0400

    ch4/ex2 Reversing an array. my version

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-function range(start, end, step = start < end ? 1 : -1) {
-  let array = [];
-
-  if (step > 0) {
-    for (let i = start; i <= end; i += step) array.push(i);
-  } else {
-    for (let i = start; i >= end; i += step) array.push(i);
+const reverseArray = (array) => {
+  let reversedArray = [];
+  let copyArray = array.slice();
+  const arrayLength = array.length;
+  for (let i = 0; i < arrayLength; i++) {
+    reversedArray.push(copyArray.pop());
   }
+
+  return reversedArray;
+};
+const reverseArrayInPlace = (array) => {
+  let reversedArray = [];
+  const arrayLength = array.length;
+  for (let i = 0; i < arrayLength; i++) {
+    reversedArray.push(array.pop());
+  }
+  array.push(...reversedArray);
   return array;
-}
+};
 
-function sum(array) {
-  let total = 0;
-  for (let value of array) {
-    total += value;
-  }
-  return total;
-}
-
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
+let myArray = ["A", "B", "C"];
+console.log(reverseArray(myArray));
+// → ["C", "B", "A"];
+console.log(myArray);
+// → ["A", "B", "C"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
