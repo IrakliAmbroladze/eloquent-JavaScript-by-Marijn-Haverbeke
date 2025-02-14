5cf5ea9078e08e49a76ca6e0c984654f297f7530
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-14 11:06:19 +0400

    ch4/ex1 The sum of a range. my version

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-let string = JSON.stringify({ squirrel: false, events: ["weekend"] });
-console.log(string);
-console.log(JSON.parse(string));
-console.log(JSON.parse(string).events);
+function range(start, end, step = 1) {
+  let allNumberArray = [];
+  if (step > 0) {
+    while (start <= end) {
+      allNumberArray.push(start);
+      start += step;
+    }
+  } else if (step < 0) {
+    while (start >= end) {
+      allNumberArray.push(start);
+      start += step;
+    }
+  } else {
+    console.log("step is zero, that is not allowed!");
+  }
+  return allNumberArray;
+}
+
+function sum(range) {
+  let total = 0;
+  for (let number of range) {
+    total += number;
+  }
+  return total;
+}
+
+console.log("range: ", range(1, 10, 2));
+console.log("sum: ", sum(range(1, 10)));
+console.log("negative range: ", range(5, 2, -1));
+console.log("sum: ", sum(range(5, 2, -1)));
+console.log("negative range: ", range(5, 2, 0));
