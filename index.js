2db4f42d616bbc794192b3a9c0881d788ecd7eae
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-25 16:06:46 +0400

    add some staff

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const createListObject = (value, rest) => ({ value, rest });
+
+let result = createListObject(1, createListObject(2, null));
+console.log(result);
+
 function arrayToList(array) {
-  let list;
-  console.log("hello");
+  let list = {};
   return list;
 }
 
-arrayToList([1, 2, 3]);
+console.log("result of arrayToList: ", arrayToList([1, 2, 3]));
+
+let testObject = { a: 1, b: 2 };
+console.log(Object.keys(testObject));
