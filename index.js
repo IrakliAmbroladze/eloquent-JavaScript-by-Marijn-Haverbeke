ea4d572e777faecb4e9c161afd0f23fc9a1d65fe
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-25 16:14:29 +0400

    transforming from array to list is done

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const createListObject = (value, rest) => ({ value, rest });
 
-let result = createListObject(1, createListObject(2, null));
-console.log(result);
-
 function arrayToList(array) {
-  let list = {};
+  let list = null;
+  for (let i = array.length; i > 0; i--) {
+    list = createListObject(i, list);
+  }
+
   return list;
 }
 
 console.log("result of arrayToList: ", arrayToList([1, 2, 3]));
-
-let testObject = { a: 1, b: 2 };
-console.log(Object.keys(testObject));
