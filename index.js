e42925bad6bd01e1ddd6638a394bf539fdfbb73a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-25 17:09:17 +0400

    ch4/ex3 A list
    My version of final solution

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const createListObject = (value, rest) => ({ value, rest });
+const prepend = (value, rest) => ({ value, rest });
 
 function arrayToList(array) {
   let list = null;
   for (let i = array.length; i > 0; i--) {
-    list = createListObject(i, list);
+    list = prepend(array[i - 1], list);
   }
 
   return list;
 }
 
-console.log("result of arrayToList: ", arrayToList([1, 2, 3]));
+let list = arrayToList([1, 2, 3]);
+console.log("result of arrayToList: ", list);
+
+function listToArray(list) {
+  let array = [];
+  while (list.rest !== null) {
+    array.push(list.value);
+    list = list.rest;
+  }
+  array.push(list.value);
+
+  return array;
+}
+
+console.log("result of listToArray: ", listToArray(list));
+
+const nth = (list, number) => {
+  let array = listToArray(list);
+  return array[number];
+};
+
+// Your code here.
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
