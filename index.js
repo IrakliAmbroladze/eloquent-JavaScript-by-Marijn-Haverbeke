9ccc5a28963bda061bdfcb403dcfea77a5fa3752
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-26 18:21:04 +0400

    ch4/ex.3 A list. book version

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-const prepend = (value, rest) => ({ value, rest });
-
 function arrayToList(array) {
   let list = null;
-  for (let i = array.length; i > 0; i--) {
-    list = prepend(array[i - 1], list);
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = { value: array[i], rest: list };
   }
-
   return list;
 }
 
-let list = arrayToList([1, 2, 3]);
-console.log("result of arrayToList: ", list);
-
 function listToArray(list) {
   let array = [];
-  while (list.rest !== null) {
-    array.push(list.value);
-    list = list.rest;
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
   }
-  array.push(list.value);
-
   return array;
 }
 
-console.log("result of listToArray: ", listToArray(list));
+function prepend(value, list) {
+  return { value, rest: list };
+}
 
-const nth = (list, number) => {
-  let array = listToArray(list);
-  return array[number];
-};
-
-// Your code here.
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
