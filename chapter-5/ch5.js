f5fad2ff5369ac8635bcf55675568a69244f4fd8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-05 09:41:13 +0400

    ch5/Composability. average year another approach

--- chapter-5/ch5.js
+++ chapter-5/ch5.js
@@ -1,13 +1,12 @@
 const SCRIPTS = require("./05_higher_order/code/scripts");
 
-const average = (array) => {
-  return array.reduce((a, b) => a + b) / array.length;
-};
+let total = 0,
+  count = 0;
+for (let script of SCRIPTS) {
+  if (script.living) {
+    total += script.year;
+    count += 1;
+  }
+}
 
-console.log(
-  Math.round(average(SCRIPTS.filter((s) => s.living).map((s) => s.year)))
-);
-
-console.log(
-  Math.round(average(SCRIPTS.filter((s) => !s.living).map((s) => s.year)))
-);
+console.log(Math.round(total / count));
