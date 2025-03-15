4c64f0b34099de8b7d52507c06561659270b809f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-15 16:23:53 +0400

    ch5/ex3-everything. part 2 my version
    using some() method

--- ex3-everything.js
+++ ex3-everything.js
@@ -8,9 +8,22 @@ function every(array, test) {
   return true;
 }
 
+function every2(array, test) {
+  // Your code here.
+
+  return array.some((x) => !test(x)) === true ? false : true;
+}
+
 console.log(every([1, 3, 5], (n) => n < 10));
 // → true
 console.log(every([2, 4, 16], (n) => n < 10));
 // → false
 console.log(every([], (n) => n < 10));
+// → true
+
+console.log(every2([1, 3, 5], (n) => n < 10));
+// → true
+console.log(every2([2, 4, 16], (n) => n < 10));
+// → false
+console.log(every2([], (n) => n < 10));
 // → true
