c846129c803390610845b5d32dabeadf08835e7b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-07 05:54:14 +0400

    ch6/ arrow this

--- chapter-6/playground.js
+++ chapter-6/playground.js
@@ -5,3 +5,11 @@ let whiteRabbit = { type: "white" };
 let hungryRabbit = { type: "hungry" };
 
 speak.call(whiteRabbit, "Hurry");
+
+let finder = {
+  find(array) {
+    return array.some((v) => v == this.value);
+  },
+  value: 4,
+};
+console.log(finder.find([4, 5])); // → true
