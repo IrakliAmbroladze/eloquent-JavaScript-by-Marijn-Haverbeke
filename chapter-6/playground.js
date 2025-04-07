6b596360bed4d937d7e3b164dd9eabde715331a6
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-07 06:41:40 +0400

    ch6/classes

--- chapter-6/playground.js
+++ chapter-6/playground.js
@@ -1,4 +1,5 @@
 let protoRabbit = {
+  a: "hello",
   speak(line) {
     console.log(`The ${this.type} rabbit says '${line}'`);
   },
@@ -8,3 +9,24 @@ let blackRabbit = Object.create(protoRabbit);
 blackRabbit.type = "black";
 
 blackRabbit.speak("I am fear and darkness");
+
+// Classes
+
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbit = new Rabbit("killer");
+
+console.log(killerRabbit);
