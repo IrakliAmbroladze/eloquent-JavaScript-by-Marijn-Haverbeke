1d7024d81a8748730f68a990bad0eaec6ed35766
Irakli <ambroladzeirakli@gmail.com>
2025-04-15 17:57:02 +0400

    9.1 Classes and Prototypes

--- chapter-6/playground-definitive-guide.js
+++ chapter-6/playground-definitive-guide.js
@@ -9,4 +9,17 @@ range.methods = {
   includes(x) {
     return this.from <= x && x <= this.to;
   },
+
+  *[Symbol.iterator]() {
+    for (let i = Math.ceil(this.from); i <= this.to; i++) yield i;
+  },
+  toString() {
+    return "(" + this.from + "..." + this.to + ")";
+  },
 };
+
+let r = range(1, 3);
+console.log(r);
+console.log(r.includes(2));
+console.log(r.toString());
+console.log([...r]);
