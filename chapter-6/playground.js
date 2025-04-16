9f206ad61fda0a981e5c818827a2f6c36c00f7bd
Irakli <ambroladzeirakli@gmail.com>
2025-04-16 18:55:53 +0400

    ch6/private properties

--- chapter-6/playground.js
+++ chapter-6/playground.js
@@ -1,25 +1,62 @@
-// const firstPrototype = {};
-// firstPrototype.color = "white";
-// const secondPrototype = Object.create(firstPrototype);
-
-// secondPrototype.style = "beautiful";
-
-// const testObject = Object.create(secondPrototype);
-
-// testObject.value = 15;
-
-// console.log(firstPrototype, secondPrototype, testObject);
-// console.log(testObject.value);
-// console.log(testObject.style);
-// console.log(testObject.color);
-
-const obj = {
-  x: 1,
-  y: "hello",
-  toString() {
-    return "This is object obj";
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
   },
 };
 
-console.log(obj + "");
-console.log(String());
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+function ArchaichRabbit(type) {
+  this.type = type;
+}
+
+ArchaichRabbit.prototype.speak = function (line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+let oldSchoolRabbit = new ArchaichRabbit("old school");
+
+oldSchoolRabbit.speak("I am from an old school!");
+
+let obj = function () {
+  return 5;
+};
+
+class SecretiveObject {
+  #getSecret() {
+    return "I ate all the plums";
+  }
+  interrogate() {
+    let shallISayIt = this.#getSecret();
+    return "never";
+  }
+}
+let secretObject = new SecretiveObject();
+
+class RandomSourse {
+  #max;
+  constructor(max) {
+    this.#max = max;
+  }
+
+  getNumber() {
+    return Math.floor(Math.random() * this.#max);
+  }
+}
+
+let random = new RandomSourse(75);
+
+console.log(random.getNumber());
