9575c047a90409668ac8e403eca8e4d40dbe5767
Irakli <ambroladzeirakli@gmail.com>
2025-04-18 10:56:53 +0400

    ch6/Iterstor

--- chapter-6/playground.js
+++ chapter-6/playground.js
@@ -92,13 +92,62 @@ class Temperature {
 }
 
 let temp = new Temperature(22);
-console.log(temp.fahrenheit);
 
 temp.fahrenheit = 86;
-console.log("temp.celsius", temp.celsius);
-console.log("temp.fahrenheit", temp.fahrenheit);
 
 let boil = Temperature.fromFahrenheit(212);
-console.log(boil.celsius);
 
-console.log("test static", temp.celsius);
+let sym = Symbol("name");
+
+let killerTabbit = new Rabbit("killer");
+
+Rabbit.prototype[sym] = 55;
+
+let okIterator = "OK"[Symbol.iterator]();
+
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+
+class List {
+  constructor(value, rest) {
+    this.value = value;
+    this.rest = rest;
+  }
+
+  get length() {
+    return 1 + (this.rest ? this.rest.length : 0);
+  }
+
+  static fromArray(array) {
+    let result = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+      result = new this(array[i], result);
+    }
+    return result;
+  }
+}
+
+class ListIterator {
+  constructor(list) {
+    this.list = list;
+  }
+  next() {
+    if (this.list == null) {
+      return { done: true };
+    }
+    let value = this.list.value;
+    this.list = this.list.rest;
+    return { value, done: false };
+  }
+}
+
+List.prototype[Symbol.iterator] = function () {
+  return new ListIterator(this);
+};
+
+let list = List.fromArray([1, 2, 3]);
+
+for (let element of list) {
+  console.log(element);
+}
