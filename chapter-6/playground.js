e59ae52defe69ec1b0bc46f3138dd8ab216f28f1
Irakli <ambroladzeirakli@gmail.com>
2025-04-18 06:19:38 +0400

    ch6/Getters, Setters, and Statics, temp celcius fahrenheit

--- chapter-6/playground.js
+++ chapter-6/playground.js
@@ -59,17 +59,46 @@ class RandomSourse {
 
 let random = new RandomSourse(75);
 
-console.log(random.getNumber());
+// let ages = new Map();
 
-console.log(obj.toString());
-console.log([1, 2].toString());
-console.log(obj.toString.call([1, 2]));
+// let myObj = { x: 1 };
 
-let ages = new Map();
+// ages.set("Irakli", 34);
+// ages.set(myObj, 45);
 
-let myObj = { x: 1 };
+// console.log(ages.get(myObj));
 
-ages.set("Irakli", 34);
-ages.set(myObj, 45);
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100);
+  },
+};
 
-console.log(ages.get(myObj));
+class Temperature {
+  constructor(celsius) {
+    this.celsius = celsius;
+  }
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
+  }
+
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+
+  static fromFahrenheit(value) {
+    return new Temperature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+
+temp.fahrenheit = 86;
+console.log("temp.celsius", temp.celsius);
+console.log("temp.fahrenheit", temp.fahrenheit);
+
+let boil = Temperature.fromFahrenheit(212);
+console.log(boil.celsius);
+
+console.log("test static", temp.celsius);
