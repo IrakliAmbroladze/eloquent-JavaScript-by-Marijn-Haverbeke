20834273f3d1ca92b8a7699d2c3cbf26674f2254
Irakli <ambroladzeirakli@gmail.com>
2025-04-19 16:02:02 +0400

    9.5.4

--- chapter-6/playground-definitive-guide.js
+++ chapter-6/playground-definitive-guide.js
@@ -1,73 +1,24 @@
-function range(from, to) {
-  let r = Object.create(range.methods);
-  r.from = from;
-  r.to = to;
-  return r;
-}
-
-range.methods = {
-  includes(x) {
-    return this.from <= x && x <= this.to;
-  },
-
-  *[Symbol.iterator]() {
-    for (let i = Math.ceil(this.from); i <= this.to; i++) yield i;
-  },
-  toString() {
-    return "(" + this.from + "..." + this.to + ")";
-  },
-};
-
-let r = range(1, 3);
-
-class Complex {
-  constructor(real, imaginary) {
-    this.r = real;
-    this.i = imaginary;
-  }
-  plus(that) {
-    return new Complex(this.r + that.r, this.i + that.i);
-  }
-  times(that) {
-    return new Complex(
-      this.r * that.r - this.i * that.i,
-      this.r * that.i + this.i * that.r
-    );
-  }
-  static sum(c, d) {
-    return c.plus(d);
-  }
-  static product(c, d) {
-    return c.times(d);
+class TypeMap extends Map {
+  constructor(keyType, valueType, entries) {
+    if (entries) {
+      for (let [k, v] of entries) {
+        if (typeof k !== keyType || typeof v !== valueType) {
+          throw new TypeError(`Wrong type for entry [${k}, ${v}]`);
+        }
+      }
+    }
+    super(entries);
+    this.keyType = keyType;
+    this.valueType = valueType;
   }
 
-  get real() {
-    return this.r;
-  }
-  get imaginary() {
-    return this.i;
-  }
-  get magnitude() {
-    return Math.hypot(this.r, this.i);
-  }
-
-  toString() {
-    return `{${this.r}, ${this.i}}`;
-  }
-
-  equals(that) {
-    return that instanceof Complex && this.r === that.r && this.i === that.i;
+  set(key, value) {
+    if (this.keyType && typeof key !== this.keyType) {
+      return new TypeError(`${key} is not of type ${this.keyType}`);
+    }
+    if (this.valueType && typeof value !== this.valueType) {
+      return new TypeError(`${value} is not of type ${this.valueType}`);
+    }
+    return super.set(key, value);
   }
 }
-
-Complex.ZERO = new Complex(0, 0);
-Complex.ONE = new Complex(1, 0);
-Complex.I = new Complex(0, 1);
-
-let c = new Complex(3, 4);
-let d = new Complex(c.i, c.r);
-console.log(c.plus(d).toString());
-console.log(c.magnitude);
-
-console.log(Complex.product(c, d).toString());
-console.log(Complex.ZERO.toString());
