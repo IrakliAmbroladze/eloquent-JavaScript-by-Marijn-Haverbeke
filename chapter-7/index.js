fa251f08f2b34441e34205c29395b7844784fdd1
Irakli <ambroladzeirakli@gmail.com>
2025-07-07 01:18:14 +0400

    create roadGraph via buildGraph function

--- chapter-7/index.js
+++ chapter-7/index.js
@@ -15,7 +15,20 @@ const roads = [
   "Shop-Town Hall",
 ];
 
-function buildGraph() {
-  let graph;
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (from in graph) {
+      graph[from].push(to);
+    } else {
+      graph[from] = [to];
+    }
+  }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
   return graph;
 }
+
+const roadGraph = buildGraph(roads);
