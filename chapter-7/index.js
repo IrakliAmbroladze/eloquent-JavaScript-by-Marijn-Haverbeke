3a20c9630e071f14732da8ea6c4c5bbdac448d13
Irakli <ambroladzeirakli@gmail.com>
2025-07-07 07:26:50 +0400

    test VillageState class

--- chapter-7/index.js
+++ chapter-7/index.js
@@ -53,3 +53,12 @@ class VillageState {
     }
   }
 }
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
